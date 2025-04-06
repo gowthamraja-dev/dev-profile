f1fa581d715617bd1338720e89d11463c0d2fe15
gowtham <gowthamraja@nextenti.ai>
2025-04-06 23:31:17 +0530

    Added Logo

--- src/Layout/BaseHeader.jsx
+++ src/Layout/BaseHeader.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ThemeToggleButton from "../ThemeProvider/ThemeToggleButton";
+import logo from "../assets/images/gowthamLogo.png";
 
 const BaseHeader = () => {
   return (
     <header className="bg-background justify-between min-h-[10vh] flex items-center px-5">
-      Header <ThemeToggleButton />
+      <img src={logo} alt="Logo" className="w-12 md:w-20 lg:w-32" />
+      <ThemeToggleButton />
     </header>
   );
 };
--- src/Layout/BaseLayout.jsx
+++ src/Layout/BaseLayout.jsx
@@ -4,14 +4,12 @@ import BaseHeader from "./BaseHeader";
 const BaseLayout = ({ children }) => {
   return (
     <div className="bg-baseBackground text-surface h-screen">
-      <div className="min-h-[10vh]">
-        <BaseHeader />
-      </div>
+      <BaseHeader />
 
-      <div className="px-4 sm:px-6 md:px-8">
-        <div className="flex flex-col md:flex-row gap-4 mt-4 min-h-[80vh]">
+      <div className="px-4 sm:px-6 md:px-8 mx-20">
+        <div className="flex flex-col md:flex-row gap-4 mt-4 min-h-[85vh]">
           {/* Right Sidebar - appears at top on mobile, right on desktop */}
-          <aside className="order-1 md:order-3 bg-background p-4 rounded-xl w-full md:w-52">
+          <aside className="order-1 bg-background p-4 rounded-xl w-full md:w-60">
             <p className="font-medium">Right Sidebar</p>
           </aside>
 
@@ -21,7 +19,7 @@ const BaseLayout = ({ children }) => {
           </main>
 
           {/* Left Sidebar - appears at bottom on mobile, left on desktop */}
-          <aside className="order-3 md:order-1 bg-background p-4 rounded-xl w-full md:w-52">
+          <aside className="order-3 bg-background p-4 rounded-xl w-full md:w-60">
             <p className="font-medium">Left Sidebar</p>
           </aside>
         </div>
