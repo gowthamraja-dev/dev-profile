80b905b7a084b882c21eec8849bda558b64d8dc3
gowtham <gowthamraja@nextenti.ai>
2025-08-30 10:55:48 +0530

    Added Images

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/dev-profile/",
+  // base: "/dev-profile/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
