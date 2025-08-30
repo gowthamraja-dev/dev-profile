83f321da77c4797e4f156db780778678fc90d8a3
gowtham <gowthamraja@nextenti.ai>
2025-08-30 12:04:51 +0530

    Removed Unwanted

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { ghPages } from "vite-plugin-gh-pages";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === "production" ? "/dev-profile/" : "/",
-  server: {
-    port: 3000,
-  },
+  plugins: [react(), tailwindcss(), ghPages()],
 });
